d3e5895ff846d0bed91f51f4adba4ff96ded2db4
Zach Likens <109999766+Zalikens@users.noreply.github.com>
2026-03-03 18:17:59 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,8 @@
+# Calculators
+
+Local web app with:
+- Margin Calculator
+- Plastic Weight Calculator
+- Cut Planner
+
+PWA-ready: includes manifest and service worker so it can be installed on iOS/Android home screens.
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+const CACHE_NAME = 'calculators-cache-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './app.js',
+  './materials.js',
+  './manifest.webmanifest',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      )
+    )
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((res) => res || fetch(event.request))
+  );
+});
